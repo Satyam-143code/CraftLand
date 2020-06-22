0f03808e0fe2e0d5868dd87635e0bb1197732479
Satyam-code143 <Satyam-code143@github.com>
2020-06-22 16:38:35 +0530

    some initial setup and redux installed

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,27 +1,14 @@
 import React, { Fragment } from "react";
-import {
-  Alignment,
-  Button,
-  Classes,
-  H5,
-  Navbar,
-  NavbarDivider,
-  NavbarGroup,
-  NavbarHeading,
-  Switch,
-} from "@blueprintjs/core";
+import { AppBar, Toolbar,Typography, Input } from "@material-ui/core";
 
 function MainPage() {
   return (
     <Fragment>
-      <Navbar>
-        <Navbar.Group align={Alignment.RIGHT}>
-          <Navbar.Heading>Blueprint</Navbar.Heading>
-          <Navbar.Divider />
-          <Button className="bp3-minimal" icon="home" text="Home" />
-          <Button className="bp3-minimal" icon="document" text="Files" />
-        </Navbar.Group>
-      </Navbar>
+      <AppBar position="static">
+        <Toolbar>
+          <Input/>
+        </Toolbar>
+      </AppBar>
     </Fragment>
   );
 }
